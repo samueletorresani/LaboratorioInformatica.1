8b8c7cff1820f930ebfd1fa28cc8aedee7d26988
Samuele Torresani <samuele.torresani01@gmail.com>
2020-10-22 15:55:33 +0200

    initial commit

---
+++ 1.0/1.1/1.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"base=";
+    cin>>a;
+    cout<<"esponente=";
+    cin>>b;
+    powint (a,b);
+    cout<<powint;
+    
+}
